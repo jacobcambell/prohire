47b83c344951b19dd3f4a26942533bcc73e36fbf
Jacob Cambell <jacob@jacobcambell.com>
2021-09-10 18:56:53 -0700

    Added api calls to admin login form, taking note that the credentials parameter in fetch() needs to be set to include for CORS/cross-domain cookies to work properly

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Admin = () => {
-    return ( 
+
+    let history = useHistory();
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleLogin = () => {
+        fetch('http://localhost:8080/adminlogin', {
+            method: 'post',
+            body: JSON.stringify({username, password}),
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            credentials: 'include'
+        })
+        .then(result => result.json())
+        .then((data) => {
+            // history.push('/');
+            console.log(data)
+        })
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:8080/adminloggedin', {
+            credentials: 'include'
+        })
+        .then(result => result.json())
+        .then((data) => {
+            console.log(data)
+        })
+    }, [])
+
+    return (
         <div className="admin">
             <p className="label">Username</p>
-            <input type="text" className="field"/>
+            <input type="text" onChange={(e) => { setUsername(e.target.value) }} className="field"/>
 
             <p className="label">Password</p>
-            <input type="password" className="field" />
+            <input type="password" onChange={(e) => { setPassword(e.target.value) }} className="field" />
 
-            <button className="submit">Login</button>
+            <button onClick={handleLogin} className="submit">Login</button>
         </div>
      );
 }
- 
+
 export default Admin;
