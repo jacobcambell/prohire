01695fda9959191ccf324ab3314d5c86bc36a124
Jacob Cambell <jacob@jacobcambell.com>
2021-09-10 19:01:22 -0700

    Added admin login functionality, also /adminloggedin to check if the user is logged in as an admin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql');
+const session = require('express-session');
 
 const con = mysql.createConnection({
     host: 'localhost',
@@ -15,10 +16,21 @@ con.connect();
 // Allow requests from localhost:3000 to this api
 const corsOptions = {
     origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
+    credentials: true
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 app.get('/getall', (req, res) => {
     con.query('SELECT id, fullname, slug FROM professionals', (err, results) => {
@@ -30,18 +42,62 @@ app.get('/getall', (req, res) => {
         })
 
         res.json(professionals);
-    });    
+    });
 })
 
 app.get('/prodetails', (req, res) => {
     // Load data for a specific professional
     let slug = req.query.slug;
 
-    con.query('SELECT fullname, location_from, profession, bio FROM professionals WHERE slug=' + con.escape(slug), (err, results) => {
+    con.query('SELECT fullname, location_from, profession, bio FROM professionals WHERE slug=?', [slug], (err, results) => {
         if (err) throw err;
 
         res.json(results[0]);
-    })    
+    })
+})
+
+app.post('/adminlogin', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // Grab the user's password
+    con.query('SELECT password FROM admins WHERE username=?', [username], (err, results) => {
+        if (err) throw err;
+
+        if(results.length == 0){
+            // No user with that username found
+            res.json({message: 'No user with that username found!'});
+            return;
+        }
+        else{
+            // Check if passwords match
+            if(results[0].password == password){
+                // Good match
+                // Note - There was an initial bug I had on this line, when setting session variables you have to set
+                // them BEFORE returning data to client (like with res.json()) I initially was setting session variables after the
+                // res.json() line and the browser was not setting cookies, likely because the data was already returned with res.json()
+                // before I had a chance to send the cookie with it
+                req.session.admin_logged_in = true;
+                res.json({message: 'Successful login'})
+                return;
+            }
+            else{
+                // Passwords don't match
+                res.json({message: 'Incorrect password!'});
+                return;
+            }
+        }
+    });
+})
+
+app.get('/adminloggedin', (req, res) => {
+    // Is the user logged in as an admin?
+    if(req.session.admin_logged_in){
+        res.json({admin_logged_in: true})
+    }
+    else{
+        res.json({admin_logged_in: false})
+    }
 })
 
 app.listen(8080, () => {
