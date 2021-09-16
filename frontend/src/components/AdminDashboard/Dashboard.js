43b80617f874d80415ea43b164a96d7d395a70fd
Jacob Cambell <jacob@jacobcambell.com>
2021-09-16 14:08:00 -0600

    If user visits admin dashboard and not admin, redirect to login

--- frontend/src/components/AdminDashboard/Dashboard.js
+++ frontend/src/components/AdminDashboard/Dashboard.js
@@ -1,6 +1,24 @@
 import styles from './css/Dashboard.module.css';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 const Dashboard = () => {
+
+    let history = useHistory();
+
+    useEffect(() => {
+        fetch('http://localhost:8080/adminloggedin', {
+            credentials: 'include'
+        })
+        .then(result => result.json())
+        .then((data) => {
+            if(!data.admin_logged_in){
+                // User is not logged in as an admin, redirect to login component
+                history.push('/admin/');
+            }
+        })
+    }, []);
+
     return (
         <div>admin dashboard</div>
      );
