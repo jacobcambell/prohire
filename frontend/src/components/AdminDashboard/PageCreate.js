f2b42071aa433e8746ff6af8fefda07091d3da47
Jacob Cambell <jacob@jacobcambell.com>
2021-09-17 13:13:22 -0600

    Design create professional page

--- frontend/src/components/AdminDashboard/PageCreate.js
+++ frontend/src/components/AdminDashboard/PageCreate.js
@@ -2,7 +2,26 @@ import styles from './css/Pages.module.css';
 
 const PageCreate = () => {
     return (
-        <div className={styles.page}></div>
+        <div className={styles.page}>
+            <p className={styles.title}>Create Professional</p>
+
+            <p className={styles.label}>Full Name</p>
+            <input type="text" className={styles.field} />
+
+            <p className={styles.label}>Location</p>
+            <input type="text" className={styles.field} />
+
+            <p className={styles.label}>Profession</p>
+            <input type="text" className={styles.field} />
+
+            <p className={styles.label}>Bio</p>
+            <textarea className={styles.field}></textarea>
+
+            <p className={styles.label}>Slug</p>
+            <input type="text" className={styles.field} />
+
+            <button className={styles.addBtn}>Create</button>
+        </div>
      );
 }
 
