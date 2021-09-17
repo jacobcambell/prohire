33c27db80d8d86c2b6f512432eb2f878959f89a8
Jacob Cambell <jacob@jacobcambell.com>
2021-09-17 11:51:05 -0600

    Load all professionals into state on render

--- frontend/src/components/AdminDashboard/PageAll.js
+++ frontend/src/components/AdminDashboard/PageAll.js
@@ -1,11 +1,24 @@
 import styles from './css/Pages.module.css';
+import { useEffect, useState } from 'react';
 
 const PageAll = () => {
+
+    const [pros, setPros] = useState();
+
+    useEffect(() => {
+        // Get all professionals
+        fetch('http://localhost:8080/getall')
+            .then(result => result.json())
+            .then(data => {
+                setPros(data);
+            })
+    }, []);
+
     return (
         <div className={styles.page}>
             <p className={styles.title}>All Professionals</p>
         </div>
-     );
+    );
 }
 
 export default PageAll;
