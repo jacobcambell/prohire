381d30a044506b38c7794ab2c41181b5a6d41cab
Jacob Cambell <jacob@jacobcambell.com>
2021-09-17 13:19:54 -0600

    Add inputs to state and add form submission handler

--- frontend/src/components/AdminDashboard/PageCreate.js
+++ frontend/src/components/AdminDashboard/PageCreate.js
@@ -1,26 +1,39 @@
 import styles from './css/Pages.module.css';
+import { useState } from 'react';
 
 const PageCreate = () => {
+
+    const [fullName, setFullName] = useState();
+    const [location, setLocation] = useState();
+    const [profession, setProfession] = useState();
+    const [bio, setBio] = useState();
+    const [slug, setSlug] = useState();
+
+    const handleForm = () => {
+
+    }
+
     return (
         <div className={styles.page}>
             <p className={styles.title}>Create Professional</p>
 
             <p className={styles.label}>Full Name</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setFullName(e.target.value) }} type="text" className={styles.field} />
 
             <p className={styles.label}>Location</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setLocation(e.target.value) }} type="text" className={styles.field} />
 
             <p className={styles.label}>Profession</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setProfession(e.target.value) }} type="text" className={styles.field} />
 
             <p className={styles.label}>Bio</p>
-            <textarea className={styles.field}></textarea>
+            <textarea onChange={(e) => { setBio(e.target.value) }} className={styles.field}></textarea>
 
             <p className={styles.label}>Slug</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setSlug(e.target.value) }} type="text" className={styles.field} />
 
-            <button className={styles.addBtn}>Create</button>
+            <button onClick={handleForm} className={styles.addBtn}>Create</button>
+
         </div>
      );
 }
