06ac5de103978cac77c65fbea0284852d6e54d70
Jacob Cambell <jacob@jacobcambell.com>
2021-09-17 21:30:14 -0600

    Design logout button in admin dashboard nav

--- frontend/src/components/AdminDashboard/Nav.js
+++ frontend/src/components/AdminDashboard/Nav.js
@@ -8,6 +8,10 @@ const Nav = () => {
 
             <Link to="/admin/dashboard/all" className={styles.tab}><i className="fas fa-user-friends"></i> All Professionals</Link>
             <Link to="/admin/dashboard/create" className={styles.tab}><i className="fa fa-plus" aria-hidden="true"></i> Create Professional</Link>
+
+            <div className={`${styles.logout} ${styles.tab}`}>
+                <i class="fas fa-sign-out-alt"></i> Logout
+            </div>
         </div>
      );
 }
