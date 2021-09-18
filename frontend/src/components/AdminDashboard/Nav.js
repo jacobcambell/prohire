4f39d6daecfad9199a26fbcc2e4e86482dcd47d3
Jacob Cambell <jacob@jacobcambell.com>
2021-09-17 21:45:58 -0600

    Add logout button functionality in admin nav

--- frontend/src/components/AdminDashboard/Nav.js
+++ frontend/src/components/AdminDashboard/Nav.js
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom';
 import styles from './css/Nav.module.css';
+import { useHistory } from 'react-router';
 
 const Nav = () => {
+
+    let history = useHistory();
+
+    const handleLogout = () => {
+        fetch('http://localhost:8080/logout', {
+            credentials: 'include'
+        })
+        .then(result => {
+            history.push('/admin');
+        })
+    }
+
     return (
         <div className={styles.nav}>
             <Link to="/admin/dashboard/all" className={styles.logo}>ProHire Admin</Link>
@@ -9,7 +22,7 @@ const Nav = () => {
             <Link to="/admin/dashboard/all" className={styles.tab}><i className="fas fa-user-friends"></i> All Professionals</Link>
             <Link to="/admin/dashboard/create" className={styles.tab}><i className="fa fa-plus" aria-hidden="true"></i> Create Professional</Link>
 
-            <div className={`${styles.logout} ${styles.tab}`}>
+            <div onClick={ handleLogout } className={`${styles.logout} ${styles.tab}`}>
                 <i class="fas fa-sign-out-alt"></i> Logout
             </div>
         </div>
