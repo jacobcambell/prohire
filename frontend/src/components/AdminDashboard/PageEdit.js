b90fc2e8e9d8194fb9b3f684b8476c838aa2b989
Jacob Cambell <jacob@jacobcambell.com>
2021-09-18 11:30:18 -0600

    Load pro details on edit page

--- frontend/src/components/AdminDashboard/PageEdit.js
+++ frontend/src/components/AdminDashboard/PageEdit.js
@@ -1,5 +1,5 @@
 import styles from './css/Pages.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const PageEdit = () => {
@@ -16,24 +16,43 @@ const PageEdit = () => {
         alert(id);
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8080/prodetailsbyid', {
+            method: 'post',
+            body: JSON.stringify({
+                id: id
+            }),
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+            .then(result => result.json())
+            .then((data) => {
+                console.log(data)
+                setFullName(data.fullname);
+                setLocation(data.location_from);
+                setProfession(data.profession);
+                setBio(data.bio);
+                setSlug(data.slug);
+            })
+    }, []);
+
     return (
         <div className={styles.page}>
-            <p className={styles.title}>Editing Professional</p>
+            <p className={styles.title}>Editing {fullName}</p>
 
             <p className={styles.label}>Full Name</p>
-            <input onChange={(e) => { setFullName(e.target.value) }} type="text" className={styles.field} />
+            <input onChange={(e) => { setFullName(e.target.value) }} value={fullName} type="text" className={styles.field} />
 
             <p className={styles.label}>Location</p>
-            <input onChange={(e) => { setLocation(e.target.value) }} type="text" className={styles.field} />
+            <input onChange={(e) => { setLocation(e.target.value) }} value={location} type="text" className={styles.field} />
 
             <p className={styles.label}>Profession</p>
-            <input onChange={(e) => { setProfession(e.target.value) }} type="text" className={styles.field} />
+            <input onChange={(e) => { setProfession(e.target.value) }} value={profession} type="text" className={styles.field} />
 
             <p className={styles.label}>Bio</p>
-            <textarea onChange={(e) => { setBio(e.target.value) }} className={styles.field}></textarea>
+            <textarea onChange={(e) => { setBio(e.target.value) }} value={bio} className={styles.field}></textarea>
 
             <p className={styles.label}>Slug</p>
-            <input onChange={(e) => { setSlug(e.target.value) }} type="text" className={styles.field} />
+            <input onChange={(e) => { setSlug(e.target.value) }} value={slug} type="text" className={styles.field} />
 
             <button onClick={handleForm} className={styles.addBtn}>Confirm Changes</button>
         </div>
