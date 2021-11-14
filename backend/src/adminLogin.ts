030febccea013deb4dbaecffb5db66a139ad4482
Jacob Cambell <jacob@jacobcambell.com>
2021-11-14 16:30:59 -0700

    Remove old AdminLogin script

--- backend/src/adminLogin.ts
+++
@@ -1,9 +0,0 @@
-export const AdminLogin = (p) => {
-    // Returns true if the provided admin password is correct, and false if incorrect
-    if (p === process.env.ADMIN_PASSWORD) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
