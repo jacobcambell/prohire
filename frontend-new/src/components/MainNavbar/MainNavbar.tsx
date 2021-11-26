454740f2add2a81bf75997c38477e5cb05f29ace
Jacob Cambell <jacob@jacobcambell.com>
2021-11-26 08:18:39 -0700

    Add chat button to nav, make background dark

--- frontend-new/src/components/MainNavbar/MainNavbar.tsx
+++ frontend-new/src/components/MainNavbar/MainNavbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { FaComments } from 'react-icons/fa'
 
 export default function MainNavbar() {
     return (
-        <div>
-            <div className="bg-black py-3 px-5">
-                <Link to="/" className="text-white text-xl">ProHire</Link>
-            </div>
+        <div className="bg-black py-3 px-10 flex justify-between items-center">
+            <Link to="/" className="text-white text-xl">ProHire</Link>
+            <button className="border border-saffron text-saffron p-2">Chat Now <FaComments className="inline"></FaComments></button>
         </div>
     )
 }
