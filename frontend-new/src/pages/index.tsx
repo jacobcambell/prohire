9bc7f2a828e60103f7f0734ceeebcd0e5a2ffaa3
Jacob Cambell <jacob@jacobcambell.com>
2021-11-26 10:52:53 -0700

    Go to correct pro page from index

--- frontend-new/src/pages/index.tsx
+++ frontend-new/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import MainNavbar from "../components/MainNavbar/MainNavbar"
 import axios from "axios"
 import { Link } from 'gatsby'
+import { FaCircle } from 'react-icons/fa'
 
 interface Pro {
   id: number;
@@ -30,9 +31,10 @@ const IndexPage = () => {
       <div className="w-3/5 m-auto text-center my-5">
         {
           pros.length > 0 && pros.map((pro) => (
-            <Link to={`/professionals/${pro.slug}`} className="m-3 w-1/4 inline-block bg-center bg-no-repeat bg-cover h-64 relative" style={{ backgroundImage: `url(${process.env.GATSBY_API_ENDPOINT}/images/${pro.image_name})` }}>
-              <div className="w-full h-1/5 bg-black bg-opacity-60 absolute bottom-0 flex items-center justify-center">
+            <Link to={`/professionals?id=${pro.id}`} className="m-3 w-1/4 inline-block bg-center bg-no-repeat bg-cover h-64 relative" style={{ backgroundImage: `url(${process.env.GATSBY_API_ENDPOINT}/images/${pro.image_name})` }}>
+              <div className="w-full h-1/5 bg-black bg-opacity-60 absolute bottom-0 flex items-center justify-between px-3">
                 <p className="text-white">{pro.fullname}</p>
+                <FaCircle className="text-green-500 text-xs" />
               </div>
             </Link>
 
